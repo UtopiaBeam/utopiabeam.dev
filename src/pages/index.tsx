f62b959afdd011c46e79bcae61f7994efd17e99b
UtopiaBeam <utopiabeam@gmail.com>
2020-05-22 18:57:48 +0700

    :truck: Move banner to a component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import SEO from '../components/SEO'
-import Logo from '../components/Logo'
 import BackgroundImage from 'gatsby-background-image'
-import { Heading, Flex } from 'rebass'
+import SEO from '../components/SEO'
+import Banner from '../components/Banner'
 
 export default () => {
   const { site, imageSharp } = useStaticQuery(graphql`
@@ -24,25 +23,7 @@ export default () => {
     <>
       <SEO />
       <BackgroundImage fluid={imageSharp.fluid}>
-        <Flex
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Logo />
-          <Heading
-            textAlign="center"
-            fontFamily="Catamaran, sans-serif"
-            fontWeight={400}
-            fontSize={[18, 22, 26]}
-            letterSpacing={2}
-            color="rgba(250, 250, 250, 0.8)"
-            mx={[3, 4, 5]}
-            mb={5}
-          >
-            {site.siteMetadata.description}
-          </Heading>
-        </Flex>
+        <Banner description={site.siteMetadata.description} />
       </BackgroundImage>
     </>
   )
