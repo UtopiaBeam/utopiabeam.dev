304d84aab9d2d25f09962068230817083a515fc1
UtopiaBeam <utopiabeam@gmail.com>
2020-06-03 16:25:27 +0700

    ⚡️ Add post page

---
+++ src/templates/Post.tsx
@@ -0,0 +1,170 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { Post } from '../types'
+import SEO from '../components/SEO'
+import { Flex, Box, Heading } from 'rebass'
+import { Global, css } from '@emotion/core'
+import Img from 'gatsby-image'
+import styled from '@emotion/styled'
+
+interface Props {
+  pageContext: {
+    slug: string
+  }
+  data: {
+    post: Post
+  }
+}
+
+const GlobalStyles = () => (
+  <Global
+    styles={css`
+      a {
+        text-decoration: none;
+        color: rgb(44, 120, 212);
+      }
+      p {
+        color: rgb(20, 20, 20);
+        @media only screen and (max-width: 420px) {
+          font-size: 18px;
+        }
+        @media (prefers-color-scheme: dark) {
+          color: rgba(230, 230, 230);
+        }
+      }
+      h1 {
+        @media (prefers-color-scheme: dark) {
+          color: rgba(230, 230, 230);
+        }
+      }
+      hr {
+        @media (prefers-color-scheme: dark) {
+          background: rgba(200, 200, 200, 0.5);
+        }
+      }
+      ul > li,
+      ol > li {
+        color: rgb(20, 20, 20);
+        @media only screen and (max-width: 420px) {
+          font-size: 18px;
+        }
+        @media (prefers-color-scheme: dark) {
+          color: rgba(230, 230, 230);
+        }
+      }
+      pre[class*='language-'] {
+        border-radius: 5px;
+        margin-bottom: 34px;
+
+        & > code {
+          font-size: 14px;
+
+          @media only screen and (max-width: 420px) {
+            font-size: 13px;
+          }
+        }
+      }
+      :not(pre) > code {
+        font-size: 18px;
+
+        @media only screen and (max-width: 420px) {
+          font-size: 17px;
+        }
+      }
+      .katex-html {
+        overflow-x: scroll;
+        @media (prefers-color-scheme: dark) {
+          color: rgba(230, 230, 230);
+        }
+      }
+      blockquote {
+        font-size: 1.2em;
+        padding: 0 1.5em;
+        border-left: 0.3em solid rgba(44, 120, 212, 0.6);
+        @media (prefers-color-scheme: dark) {
+          border-left: 0.3em solid rgba(44, 120, 212, 0.8);
+        }
+        @media only screen and (max-width: 420px) {
+          padding: 0 0.75em;
+          margin-left: 0;
+          margin-right: 0;
+        }
+      }
+      table {
+        table-layout: fixed;
+        & > tbody {
+          font-size: 0.85em;
+        }
+        @media (prefers-color-scheme: dark) {
+          color: rgba(230, 230, 230);
+        }
+      }
+    `}
+  />
+)
+
+const Title = styled(props => (
+  <Heading
+    {...props}
+    fontFamily="Kanit, sans-serif"
+    fontWeight={700}
+    fontSize={[36, 40, 44]}
+    pb={3}
+  />
+))`
+  @media (prefers-color-scheme: dark) {
+    color: rgb(245, 245, 245);
+  }
+`
+
+export default ({ data: { post } }: Props) => {
+  return (
+    <>
+      <GlobalStyles />
+      <SEO
+        title={post.title}
+        slug={post.slug}
+        description={post.description}
+        date={post.createdAt}
+        banner={post.banner.fluid.src}
+      />
+      <Flex justifyContent="center">
+        <Box width={[4 / 5, 3 / 4, 1 / 2]} py={4}>
+          <Title>{post.title}</Title>
+          <Img fluid={post.banner.fluid} alt="banner" />
+          <Box py={4}>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: post.content.childMarkdownRemark.html!,
+              }}
+            />
+          </Box>
+        </Box>
+      </Flex>
+    </>
+  )
+}
+
+export const pageQuery = graphql`
+  query postQuery($slug: String!) {
+    post: contentfulPost(slug: { eq: $slug }) {
+      title
+      description
+      createdAt(formatString: "DD MMM YYYY")
+      content {
+        childMarkdownRemark {
+          html
+        }
+      }
+      banner {
+        fluid(maxWidth: 1000, quality: 80) {
+          ...GatsbyContentfulFluid
+        }
+      }
+      tags {
+        name
+        slug
+      }
+    }
+  }
+`
