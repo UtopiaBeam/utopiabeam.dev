6acaadce2813bbccef1ca874cdd363226c6afa17
UtopiaBeam <utopiabeam@gmail.com>
2020-08-01 15:10:10 +0700

    ♻️ Use date toLocaleDateString function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,19 +1,8 @@
 export function getDateString(dateStr: string) {
   const date = new Date(dateStr)
-  const months = [
-    'ม.ค.',
-    'ก.พ.',
-    'มี.ค.',
-    'เม.ษ.',
-    'พ.ค.',
-    'มิ.ย.',
-    'ก.ค.',
-    'ส.ค.',
-    'ก.ย.',
-    'ต.ค.',
-    'พ.ย.',
-    'ธ.ค.',
-  ]
-  return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear() +
-    543}`
+  return date.toLocaleDateString('th-TH', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  })
 }
