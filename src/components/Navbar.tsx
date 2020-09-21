7a39472889fd8b25ee3fb610528223bf189103c7
UtopiaBeam <utopiabeam@gmail.com>
2020-09-21 18:43:14 +0700

    ✨ Add about me

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,6 +56,10 @@ export default ({ children }: Props) => {
       name: 'Blog',
       href: '/blog',
     },
+    {
+      name: 'About Me',
+      href: '/me'
+    }
   ]
 
   const navTabs = tabs.map((tab, i) => (
