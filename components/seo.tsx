f79ef10ff7d8535e761422ad10d011bc05e7a8dd
Natchapol Srisang <utopiabeam@gmail.com>
2021-02-14 10:21:07 +0700

    🔧 Fix wrong fonts config

--- components/seo.tsx
+++ components/seo.tsx
@@ -58,15 +58,9 @@ const SEO: React.FC<Props> = props => {
       <meta name="og:locale" content="th_TH" />
       <meta name="fb:app_id" content="100001227099580" />
 
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Sarabun:ital,wght@0,400;0,600;1,400&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        href="https://fonts.googleapis.com/earlyaccess/notosansthai.css"
-        ref="stylesheet"
-      />
+      {props.children}
     </Head>
   )
 }
+
+export default SEO
