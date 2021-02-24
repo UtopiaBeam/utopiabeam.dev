ab264e252e62b1c27d98edd71baa920d689b5249
Natchapol Srisang <utopiabeam@gmail.com>
2021-02-24 17:56:42 +0700

    💄 Change nav text to orange on hover

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Nav: React.FC = () => {
   return (
-    <nav className="flex space-x-6 items-baseline p-3 bg-gray-200 dark:bg-darkgray border-b-2 border-darkgray dark:border-white border-opacity-10">
+    <nav className="flex space-x-8 items-baseline p-3 bg-gray-200 dark:bg-darkgray border-b-2 border-darkgray dark:border-white border-opacity-10">
       <Link href="/">
         <div className="flex items-center pr-4 cursor-pointer">
           <h3 className="text-orange font-logo font-bold select-none">{'{'}</h3>
@@ -13,12 +13,12 @@ const Nav: React.FC = () => {
         </div>
       </Link>
       <Link href="/blog">
-        <p className="font-mono dark:text-gray-200 select-none cursor-pointer">
+        <p className="font-mono dark:text-gray-200 select-none cursor-pointer hover:text-orange">
           BLOG
         </p>
       </Link>
       <Link href="/me">
-        <p className="font-mono dark:text-gray-200 select-none cursor-pointer">
+        <p className="font-mono dark:text-gray-200 select-none cursor-pointer hover:text-orange">
           ABOUT ME
         </p>
       </Link>
