657df75fb4a0bd3ab84c0e46021926d236ef241d
Natchapol Srisang <utopiabeam@gmail.com>
2021-02-24 17:18:18 +0700

    ✨ Add posts page

---
+++ components/pagination.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+
+interface Props {
+  current: number
+  total: number
+}
+
+const Pagination: React.FC<Props> = ({ current, total }) => {
+  const start = Math.max(current - 2, 1)
+  const end = Math.min(current + 2, total)
+
+  const pages = Array.from({ length: end - start + 1 }, (_, i) => {
+    const page = start + i
+
+    return (
+      <Link key={`page-${page}`} href={page === 1 ? '/blog' : `/blog/${page}`}>
+        <p
+          className={`dark:${
+            page === current ? 'text-blue-500' : 'text-gray-500'
+          } dark:hover:text-orange cursor-pointer`}
+        >
+          {page}
+        </p>
+      </Link>
+    )
+  })
+
+  return <div className="flex justify-center space-x-8">{pages}</div>
+}
+
+export default Pagination
---
+++ pages/blog/[[...page]].tsx
@@ -0,0 +1,50 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { getPosts, Post } from '../../services'
+import Posts from '../../components/posts'
+import Pagination from '../../components/pagination'
+import SEO from '../../components/seo'
+
+interface Props {
+  posts: Post[]
+  page: number
+  totalPage: number
+}
+
+const Page: React.FC<Props> = ({ posts, page, totalPage }) => {
+  return (
+    <>
+      <SEO title="Blog" />
+      <div className="flex flex-col px-4 md:px-8 lg:px-16 py-6 space-y-6 dark:bg-darkgray">
+        <h1 className="dark:text-gray-200">ALL POSTS</h1>
+        <Posts posts={posts} />
+        <Pagination current={page} total={totalPage} />
+      </div>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps<Props> = async ctx => {
+  const { params, preview = false } = ctx
+  const page = Number(params.page?.[0] ?? 1)
+
+  const allPosts = await getPosts(preview)
+  const posts = allPosts.slice(6 * (page - 1), 6 * page)
+  const totalPage = Math.ceil(allPosts.length / 6)
+
+  return { props: { posts, page, totalPage } }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await getPosts()
+
+  const pageCount = Math.ceil(posts.length / 6)
+
+  return {
+    paths: Array.from({ length: pageCount }, (_, page) => ({
+      params: { page: page === 0 ? [] : [(page + 1).toString()] },
+    })),
+    fallback: false,
+  }
+}
+
+export default Page
