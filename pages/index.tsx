a87f9d0979a7eff52a3ef07d286c58ac09626a5b
Natchapol Srisang <utopiabeam@gmail.com>
2021-02-24 17:17:47 +0700

    ♻️ Remove wrapper div

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
 import Posts from '../components/posts'
 import SEO from '../components/seo'
 import { getPosts, Post } from '../services'
@@ -14,17 +14,15 @@ const Index: NextPage<Props> = props => {
     <>
       <SEO />
       <div className="flex flex-col px-4 md:px-8 lg:px-16 py-6 space-y-6 dark:bg-darkgray">
-        <div>
-          <h1 className="dark:text-gray-200 mb-4">RECENT POSTS</h1>
-          <Posts posts={posts} />
-        </div>
+        <h1 className="dark:text-gray-200">RECENT POSTS</h1>
+        <Posts posts={posts} />
       </div>
     </>
   )
 }
 
-export async function getStaticProps() {
-  const posts = (await getPosts()).slice(0, 6)
+export const getStaticProps: GetStaticProps<Props> = async ctx => {
+  const posts = (await getPosts(ctx.preview)).slice(0, 6)
 
   return { props: { posts } }
 }
