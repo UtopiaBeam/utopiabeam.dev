3ef12474a4c0ea4e5dd6482c15c834e1e6424051
Natchapol Srisang <utopiabeam@gmail.com>
2021-06-13 09:31:00 +0700

    fix(404): fix style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,11 @@ import '../styles/katex.min.css'
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   const router = useRouter()
 
-  return (
+  return router.pathname === '/404' ? (
+    <Component {...pageProps} />
+  ) : (
     <div className="flex flex-col dark:bg-darkgray min-h-screen">
-      {router.pathname !== '/404' ? <Nav /> : null}
+      <Nav />
       <div className="flex flex-col flex-grow px-4 md:px-8 lg:px-16 py-6 space-y-6">
         <Component {...pageProps} />
       </div>
