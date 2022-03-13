d965874d23459f236617b546b7a102e420b5ae53
UtopiaBeam <utopiabeam@gmail.com>
2022-03-13 15:16:50 +0700

    feat: update posts layout

--- components/posts.tsx
+++ components/posts.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Posts: React.FC<Props> = ({ posts }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {posts.map(post => (
         <Card
           key={`card-${post.slug}`}
