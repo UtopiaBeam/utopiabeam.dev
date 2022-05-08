782861e20abd5ffd682af76d37d87014d5b4da12
UtopiaBeam <utopiabeam@gmail.com>
2022-05-08 12:15:50 +0700

    feat(card): use twitch hover effect

--- components/card.tsx
+++ components/card.tsx
@@ -14,16 +14,16 @@ interface Props {
 const Card: React.FC<Props> = props => {
   const { banner } = props
 
-  const card = (
-    <div className="flex flex-col ease-in-out duration-100 border-2 border-gray-400/50 rounded shadow-[0_0_orange] hover:translate-x-1 hover:-translate-y-1 hover:border-orange hover:shadow-[-0.25rem_0.25rem_orange]">
-      {banner ? (
+  return (
+    <div>
+      <div className="thumbnail">
         <Image
+          layout="responsive"
           src={banner.url}
           width={banner.width}
           height={banner.height}
-          className="rounded-t-sm"
         />
-      ) : null}
+      </div>
       <div className="flex flex-col px-4 py-3 space-y-3">
         <p className="dark:text-gray-500">{formatDate(props.date)}</p>
         <div className="space-x-3 font-mono dark:text-blue-500">
@@ -33,13 +33,15 @@ const Card: React.FC<Props> = props => {
             </span>
           ))}
         </div>
-        <h2 className="dark:text-gray-100">{props.title}</h2>
+        <Link href={props.url}>
+          <h2 className="dark:text-gray-100 hover:dark:text-orange">
+            {props.title}
+          </h2>
+        </Link>
         <p className="dark:text-gray-400">{props.description}</p>
       </div>
     </div>
   )
-
-  return props.url ? <Link href={props.url}>{card}</Link> : card
 }
 
 export default Card
