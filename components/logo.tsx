e4ad97408cb661d6043bd323ab00112afdd917ed
UtopiaBeam <utopiabeam@gmail.com>
2022-05-14 17:14:49 +0700

    fix(logo): fix logo style

---
+++ components/logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Logo: React.FC = () => {
+  return (
+    <div className="font-logo font-bold text-xl select-none">
+      <span className="text-orange">{'{'}</span>
+      <span className="text-white">UtopiaBeam</span>
+      <span className="text-orange">{'}'}</span>
+    </div>
+  )
+}
+
+export default Logo
