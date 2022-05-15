6df5e929cd93c7e2346e97f27b06ff6a77c01dbc
UtopiaBeam <utopiabeam@gmail.com>
2022-05-15 14:57:57 +0700

    feat(experience): add external link

--- components/buttons/external-link.tsx
+++ components/buttons/external-link.tsx
@@ -8,16 +8,14 @@ const ExternalLink: React.FC<Props> = ({ url }) => (
   <Link href={url}>
     <a target="_blank" className="select-none cursor-pointer">
       <svg
-        role="img"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         width={24}
         height={24}
-        className="fill-current dark:text-gray-200 hover:text-orange"
+        className="fill-current dark:text-gray-500 hover:text-orange"
       >
-        <g transform="matrix(1,0,0,-1,30.372881,1426.9492)">
-          <path d="M 1408,608 V 288 Q 1408,169 1323.5,84.5 1239,0 1120,0 H 288 Q 169,0 84.5,84.5 0,169 0,288 v 832 Q 0,1239 84.5,1323.5 169,1408 288,1408 h 704 q 14,0 23,-9 9,-9 9,-23 v -64 q 0,-14 -9,-23 -9,-9 -23,-9 H 288 q -66,0 -113,-47 -47,-47 -47,-113 V 288 q 0,-66 47,-113 47,-47 113,-47 h 832 q 66,0 113,47 47,47 47,113 v 320 q 0,14 9,23 9,9 23,9 h 64 q 14,0 23,-9 9,-9 9,-23 z m 384,864 V 960 q 0,-26 -19,-45 -19,-19 -45,-19 -26,0 -45,19 L 1507,1091 855,439 q -10,-10 -23,-10 -13,0 -23,10 L 695,553 q -10,10 -10,23 0,13 10,23 l 652,652 -176,176 q -19,19 -19,45 0,26 19,45 19,19 45,19 h 512 q 26,0 45,-19 19,-19 19,-45 z" />
-        </g>
+        <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+        <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
       </svg>
     </a>
   </Link>
