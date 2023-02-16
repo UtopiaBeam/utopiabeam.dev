5815beadd600e62a2b3c29ac0102eedea61e5734
Natchapol Srisang <natchapol@brikl.com>
2023-02-16 14:19:45 +0700

    fix(ga): fix google analytics not working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 import { AnalyticsWrapper } from '../components/analytics'
 import Footer from '../components/footer'
 import Nav from '../components/nav'
@@ -13,6 +14,18 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
     <Component {...pageProps} />
   ) : (
     <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=G-2L4441YPR0`}
+      ></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-2L4441YPR0');
+    `}
+      </Script>
       <div className="flex flex-col bg-darkgray min-h-screen">
         <Nav />
         <div className="flex flex-col flex-grow px-4 md:px-8 lg:px-16 py-6 space-y-6">
